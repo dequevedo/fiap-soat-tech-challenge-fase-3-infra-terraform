ddee3f3fb142f7441e6647a3ae11dc6ebdeb58d3
Renato Martins <70973620+RenatoMartinsXrd@users.noreply.github.com>
2025-03-20 01:40:21 -0300

    Update vpc.tf

--- terraform-eks/vpc.tf
+++ terraform-eks/vpc.tf
@@ -2,15 +2,15 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.19.0"
 
-  name = local.name
-  cidr = local.vpc_cidr
+  name = "tech-challenge-vpc"
+  cidr = "10.0.0.0/22"  # Mesmo range da VPC existente
 
-  azs             = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.2.0/26", "10.0.2.64/26", "10.0.2.128/26"]
+  public_subnets  = ["10.0.0.0/26", "10.0.0.64/26", "10.0.0.128/26"]
 
   enable_nat_gateway = true
+  single_nat_gateway = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
