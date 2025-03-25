c29b1c87395e4a4c67e844a712080b58ba64cf37
Renato Martins <70973620+RenatoMartinsXrd@users.noreply.github.com>
2025-03-25 05:11:09 -0300

    Delete terraform-eks/variables.tf

--- terraform-eks/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "s3_bucket" {
-  description = "Nome do bucket S3 para armazenar o state do Terraform"
-  type        = string
-}
-
-variable "dynamodb_table" {
-  description = "Nome da tabela DynamoDB para locking do Terraform"
-  type        = string
-}
