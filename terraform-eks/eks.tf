ac26d8c9a2164c23ffbf33106b03287f9f8f2c87
Renato Martins <70973620+RenatoMartinsXrd@users.noreply.github.com>
2025-03-25 02:44:47 -0300

    Update eks.tf

--- terraform-eks/eks.tf
+++ terraform-eks/eks.tf
@@ -28,7 +28,7 @@ module "eks" {
   eks_managed_node_groups = {
     amc-cluster-wg = {
       min_size     = 1
-      max_size     = 2
+      max_size     = 3
       desired_size = 1
 
       instance_types = ["t3.medium"]
